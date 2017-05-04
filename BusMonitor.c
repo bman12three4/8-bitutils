61cb1cae4eaebadadfe3eab66b60ef756ac7911a
im <byron.lathi@student.lovett.org>
2017-05-04 08:07:57 -0400

    finished serial code

--- BusMonitor.c
+++ BusMonitor.c
@@ -10,6 +10,12 @@
 
 #include<avr/io.h>
 #include<avr/interrupt.h>
+#include<avr/iom162.h>
+
+#define FOSC 8000000		 //Clock Speed
+#define BAUD 9600   		 //Baud
+#define myUBRR FOSC/16/BAUD-1    //Baud Rate Register value
+
 
 ISR(INT0_vect){
 	//do something
@@ -19,5 +25,18 @@ ISR(INT0_vect){
 }
 
 void main(){
+	cli();
+	USART_init(myUBRR);
 	sei();
+	
+}
+
+void USART_init(unsigned int ubrr){
+
+	UBRR0H = (unsigned char) (ubrr >> 8);	//set UBRRH to the first 8 bits of ubrr
+	UBRR0L = (unsigned char) ubrr;		//set UBRRL to the last 8 bits of ubrr
+
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0); 	//Enable receiver and transmiter
+
+	UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);	//set frame format: 8 data 2 stop bit.	
 }
