04c127f89eeb772ab186e6542b59a05c72516cc0
im <byron.lathi@student.lovett.org>
2017-05-04 08:57:27 -0400

    Added serial transmit

--- BusMonitor.c
+++ BusMonitor.c
@@ -3,9 +3,9 @@
  *	May 3 2017
  *	Purpose: To act as a monitor for the 8-bit computer that I am building.
  * 		 I should be able to trigger the MCU either by sending a serial 
- *		 command or by activating an interupt. It should then send back
- * 		 over serial the contents of the address and data busses. It
- *		 might also have a mode where it constantly does this.
+ *		 command.  It should then send back over serial the contents of 
+ *		 the address and data busses. It might also have a mode where it 
+ *	   	 constantly does this.
  */
 
 #include<avr/io.h>
@@ -17,12 +17,6 @@
 #define myUBRR FOSC/16/BAUD-1    //Baud Rate Register value
 
 
-ISR(INT0_vect){
-	//do something
-	//This is really just here for testing so that I can manually
-	//activate it, in case the serial thing does not receive. The goal
-	//is to have this all done over serial
-}
 
 void main(){
 	cli();
@@ -39,4 +33,17 @@ void USART_init(unsigned int ubrr){
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0); 	//Enable receiver and transmiter
 
 	UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);	//set frame format: 8 data 2 stop bit.	
+}
+
+void USART_transmit(unsigned int data){
+	while (!(UCSR0A & (1<<UDRE0)))
+		;
+	
+	UCSR0B &= ~(1<<TXB80);
+	
+	if (data & 0x0100){
+		UCSR0B |= 1<<TXB80;
+	}
+
+	UDR0 = data;
 }
